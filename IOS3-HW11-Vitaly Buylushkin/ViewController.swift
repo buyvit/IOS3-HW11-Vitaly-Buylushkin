df25c4a73705537330e5d105cb83286824d6f709
Vitaly Buylushkin <vitalybuylushkin@yandex.ru>
2021-10-04 20:20:06 +0300

    Create UI
    Creating UI views for timer.

--- IOS3-HW11-Vitaly Buylushkin/ViewController.swift
+++ IOS3-HW11-Vitaly Buylushkin/ViewController.swift
@@ -8,10 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var isWorkTime = true
+    private var isStarted = false
+    private var count = 1500
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBOutlet weak var myProgressView: UIProgressView!
+    
+    @IBOutlet weak var myButton: UIButton!
+    
+    
+    @IBAction func myButtonPressed(_ sender: Any) {
+        isStarted = !isStarted
+        if isStarted {
+            self.myButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        } else {
+            self.myButton.setImage(UIImage(systemName: "play"), for: .normal)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        self.myProgressView.progress = 0.0
+        self.myButton.setImage(UIImage(systemName: "play"), for: .normal)
+        if count == 1500 {
+            isWorkTime = true
+            self.myButton.setTitleColor(UIColor.red, for: .normal)
+            self.myLabel.textColor = UIColor.red
+            self.myProgressView.progressTintColor = UIColor.red
+        } else {
+            isWorkTime = false
+            self.myButton.setTitleColor(UIColor.green, for: .normal)
+            self.myLabel.textColor = UIColor.green
+            self.myProgressView.progressTintColor = UIColor.green
+        }
     }
 
 
